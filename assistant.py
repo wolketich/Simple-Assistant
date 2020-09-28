1330de2b51709d0c4628069a58205bb521f848f2
Vladislav Cernega <vladislav@walkcat.net>
2020-09-29 00:00:00 +0100

    Commit 2020-09-29 - 20 files updated.

--- assistant.py
+++ assistant.py
@@ -2,3 +2,8 @@ import pyttsx3,datetime,os,random,requests
 import wikipedia,webbrowser,sys,pywhatkit
 import speech_recognition as sr 
 
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty("voices")
+engine.setProperty('voice',voices[1].id)
+
